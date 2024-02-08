4b2827c322154072e6466c7b57ea6c9ac9079177
Sheron <sheron_carneiro@estudante.sesisenai.org.br>
2024-02-08 11:53:24 -0300

    js

--- voto_main.js
+++ voto_main.js
@@ -16,4 +16,13 @@ cidadao.titulo = titulo
 
 console.log('instância do ebjeto:',cidadao)
 
+let mensagem = cidadao.verificaVoto()
+
+resposta.innerHTML = ''
+
+resposta.innerHTML += `O eleitor ${cidadao.nome} <br>`
+resposta.innerHTML += `tem ${cidadao.idade} anos de idade <br>`
+resposta.innerHTML += mensagem +'<br>'
+resposta.style.color ="blueviolet"
+resposta.style.fontSize ="1.4rem "
 })
